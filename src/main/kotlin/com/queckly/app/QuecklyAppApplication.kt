57697d4961cb1bc6b8bce5b2a0fac8643a48c72a
Łukasz Widawski <lk.widawski@gmail.com>
2022-12-07 14:16:15 +0100

    [3] Optimizing security for Postman

--- src/main/kotlin/com/queckly/app/QuecklyAppApplication.kt
+++ src/main/kotlin/com/queckly/app/QuecklyAppApplication.kt
@@ -1,9 +1,11 @@
 package com.queckly.app
 
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@AutoConfiguration
 class QuecklyAppApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/queckly/app/controller/GraphqlSchema.kt
+++ src/main/kotlin/com/queckly/app/controller/GraphqlSchema.kt
@@ -1,12 +1,11 @@
 package com.queckly.app.controller
 
+
 import com.expediagroup.graphql.server.operations.Query
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.CrossOrigin
 
 @Suppress("unused")
 @Component
-@CrossOrigin(origins = ["http://localhost:8080"])
 class GraphqlSchema : Query {
     fun hello(): String = "Hello Kotlin!"
 }
