91dbcbba7bf604ef5367e2b5e05f32950074eb1d
Łukasz Widawski <lk.widawski@gmail.com>
2022-12-09 00:16:17 +0100

    [3] Initial setup completed
    - okta security
    - graphql
    - adding PKCE to the auth flow

---
+++ src/main/kotlin/com/queckly/app/ServletInitializer.kt
@@ -0,0 +1,13 @@
+package com.queckly.app
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+@Suppress("unused")
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(QuecklyAppApplication::class.java)
+	}
+
+}
---
+++ src/main/kotlin/com/queckly/app/controller/PublicController.kt
@@ -0,0 +1,14 @@
+package com.queckly.app.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/public")
+class PublicController {
+
+    @GetMapping
+    fun greetings(): String = "Welcome to Queckly. Building in progress."
+
+}
